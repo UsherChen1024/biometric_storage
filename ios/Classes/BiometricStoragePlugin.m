b024da2aa09dc8ff1cd8ae27681a78c3dcf53336
付贵 <fugui13@jd.com>
2023-11-13 20:29:54 +0800

    去掉没用上swift文件

--- ios/Classes/BiometricStoragePlugin.m
+++ ios/Classes/BiometricStoragePlugin.m
@@ -1,10 +1,8 @@
 #import "BiometricStoragePlugin.h"
-#import <joy_biometric_storage/joy_biometric_storage-Swift.h>
 #import "BiometricStorageImp.h"
 
 @implementation BiometricStoragePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-//  [SwiftBiometricStoragePlugin registerWithRegistrar:registrar];
     FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:@"biometric_storage" binaryMessenger:[registrar messenger]];
     BiometricStoragePlugin *instance = [[BiometricStoragePlugin alloc] init];
     [registrar addMethodCallDelegate:instance channel:channel];
